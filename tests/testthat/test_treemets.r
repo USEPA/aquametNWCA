441189fbc3ac640863e6cdef941090abdbbbb0c2
Karen Blocksom <blocksom.karen@epa.gov>
2021-01-08 14:21:15 -0800

    Updated tests to remove contributed package functions. Updated function imports.

--- tests/testthat/test_treemets.r
+++ tests/testthat/test_treemets.r
@@ -10,7 +10,7 @@ test_that("Snag metric values correct",
           {
            snagOut <- calcSnagMets(testTree,nplots,sampID='UID')
            compOut <- merge(testMets,snagOut,by=c('UID','PARAMETER'))
-           compOut <- dplyr::mutate(compOut,RESULT.x=as.numeric(RESULT.x))
+           compOut$RESULT.x <- with(compOut,as.numeric(RESULT.x))
            expect_true(nrow(snagOut)==140)
            expect_true(nrow(compOut)==nrow(snagOut))
            expect_equal(as.numeric(compOut$RESULT.x),as.numeric(compOut$RESULT.y),tolerance=0.001)            
@@ -20,7 +20,7 @@ test_that("Tree count metric values correct",
           {
            tcntOut <- calcTreeCntMets(testTree,nplots,sampID='UID')
            compOut <- merge(testMets,tcntOut,by=c('UID','PARAMETER'))
-           compOut <- dplyr::mutate(compOut,RESULT.x=as.numeric(RESULT.x))
+           compOut$RESULT.x <- with(compOut,as.numeric(RESULT.x))
            expect_true(nrow(tcntOut)==220)
            expect_true(nrow(compOut)==nrow(tcntOut))
            expect_equal(as.numeric(compOut$RESULT.x),as.numeric(compOut$RESULT.y),tolerance=0.001)                      
@@ -30,7 +30,7 @@ test_that("Tree cover metric values correct",
           {
            tcvrOut <- calcTreeCoverMets(testTree,nplots,sampID='UID')
            compOut <- merge(testMets,tcvrOut,by=c('UID','PARAMETER'))
-           compOut <- dplyr::mutate(compOut,RESULT.x=as.numeric(RESULT.x))
+           compOut$RESULT.x <- with(compOut,as.numeric(RESULT.x))
            expect_true(nrow(tcvrOut)==400)
            expect_true(nrow(compOut)==nrow(tcvrOut))
            expect_equal(as.numeric(compOut$RESULT.x),as.numeric(compOut$RESULT.y),tolerance=0.001)            
@@ -40,9 +40,12 @@ test_that("Tree cover metric values correct",
 test_that("All tree metric values correct",
           {
             treeOut <- calcTreeMets(testTree,nplots,sampID='UID')
-            treeOut.long <- reshape2::melt(treeOut,id.vars=c('UID'),variable.name='PARAMETER',value.name='RESULT')
+            varNames <- names(treeOut)[names(treeOut)!='UID']
+            treeOut.long <- reshape(treeOut, idvar = 'UID', direction = 'long',
+                                    times = varNames, varying = varNames,
+                                    timevar = 'PARAMETER', v.names = 'RESULT')
             compOut <- merge(testMets,treeOut.long,by=c('UID','PARAMETER'))
-            compOut <- dplyr::mutate(compOut,RESULT.x=as.numeric(RESULT.x))
+            compOut$RESULT.x <- with(compOut,as.numeric(RESULT.x))
             expect_true(nrow(treeOut.long)==760)
             expect_true(nrow(compOut)==nrow(treeOut.long))
             expect_equal(as.numeric(compOut$RESULT.x),as.numeric(compOut$RESULT.y),tolerance=0.001)                      
--- tests/testthat/test_vascmets.r
+++ tests/testthat/test_vascmets.r
@@ -22,7 +22,7 @@ test_that("Richness metric values are correct",
           {
           richOut <- calcRichness(dfTest[[1]],dfTest[[2]],dfTest[[3]],dfTest[[4]],dfTest[[5]],dfTest[[6]])
            compOut <- merge(testMets,richOut,by=c('UID','PARAMETER'))
-           compOut <- dplyr::mutate(compOut,RESULT.x=as.numeric(RESULT.x))
+           compOut$RESULT.x <- with(compOut, as.numeric(RESULT.x))
            expect_true(nrow(compOut)==nrow(richOut))
            expect_equal(compOut$RESULT.x,compOut$RESULT.y,tolerance=0.001)
 })
@@ -37,7 +37,7 @@ test_that("Category metric function values correct",
           {
           catOut <- calcCategory(testForCalc)
            compOut <- merge(testMets,catOut,by=c('UID','PARAMETER'))
-           compOut <- dplyr::mutate(compOut,RESULT.x=as.numeric(RESULT.x))
+           compOut$RESULT.x <- with(compOut, as.numeric(RESULT.x))
            expect_true(nrow(catOut)==720)
            expect_true(nrow(compOut)==nrow(catOut))
            expect_equal(compOut$RESULT.x,compOut$RESULT.y,tolerance=0.001)
@@ -47,7 +47,7 @@ test_that("Duration metric function values correct",
           {
           durOut <- calcDuration(testForCalc)
            compOut <- merge(testMets,durOut,by=c('UID','PARAMETER'))
-           compOut <- dplyr::mutate(compOut,RESULT.x=as.numeric(RESULT.x))
+           compOut$RESULT.x <- with(compOut, as.numeric(RESULT.x))
            expect_true(nrow(durOut)==480)
            expect_true(nrow(compOut)==nrow(durOut))
            expect_equal(compOut$RESULT.x,compOut$RESULT.y,tolerance=0.001)
@@ -57,7 +57,7 @@ test_that("Growth habit metric function values correct",
           {
           grhOut <- calcGrowthHabit(testForCalc)
            compOut <- merge(testMets,grhOut,by=c('UID','PARAMETER'))
-           compOut <- dplyr::mutate(compOut,RESULT.x=as.numeric(RESULT.x))
+           compOut$RESULT.x <- with(compOut, as.numeric(RESULT.x))
            expect_true(nrow(grhOut)==1160)
            expect_true(nrow(compOut)==nrow(grhOut))
            expect_equal(compOut$RESULT.x,compOut$RESULT.y,tolerance=0.001)
@@ -67,7 +67,7 @@ test_that("WIS metric function values correct",
           {
           wisOut <- calcWIS(testForCalc)
            compOut <- merge(testMets,wisOut,by=c('UID','PARAMETER'))
-           compOut <- dplyr::mutate(compOut,RESULT.x=as.numeric(RESULT.x))
+           compOut$RESULT.x <- with(compOut, as.numeric(RESULT.x))
            expect_true(nrow(wisOut)==430)
            expect_true(nrow(compOut)==nrow(wisOut))
            expect_equal(compOut$RESULT.x,compOut$RESULT.y,tolerance=0.001)
@@ -77,7 +77,7 @@ test_that("CC metric function values correct",
           {
           ccOut <- calcCC(testForCalc)
            compOut <- merge(testMets,ccOut,by=c('UID','PARAMETER'))
-           compOut <- dplyr::mutate(compOut,RESULT.x=as.numeric(RESULT.x))
+           compOut$RESULT.x <- with(compOut, as.numeric(RESULT.x))
            expect_true(nrow(ccOut)==320)
            expect_true(nrow(compOut)==nrow(ccOut))
            expect_equal(compOut$RESULT.x,compOut$RESULT.y,tolerance=0.001)
@@ -87,7 +87,7 @@ test_that("Native metric function values correct",
           {
           natOut <- calcNative(testForCalc)
            compOut <- merge(testMets,natOut,by=c('UID','PARAMETER'))
-           compOut <- dplyr::mutate(compOut,RESULT.x=as.numeric(RESULT.x))
+           compOut$RESULT.x <- with(compOut, as.numeric(RESULT.x))
            expect_true(nrow(natOut)==300)
            expect_true(nrow(compOut)==nrow(natOut))
            expect_equal(compOut$RESULT.x,compOut$RESULT.y,tolerance=0.001)
@@ -97,7 +97,7 @@ test_that("Diversity metric function values correct",
           {
           divOut <- calcDiversity(testForCalc)
            compOut <- merge(testMets,divOut,by=c('UID','PARAMETER'))
-           compOut <- dplyr::mutate(compOut,RESULT.x=as.numeric(RESULT.x))
+           compOut$RESULT.x <- with(compOut, as.numeric(RESULT.x))
            expect_true(nrow(divOut)==120)
            expect_true(nrow(compOut)==nrow(divOut))
            expect_equal(compOut$RESULT.x,compOut$RESULT.y,tolerance=0.001)
@@ -107,7 +107,7 @@ test_that("Mean Bray-Curtis metric values correct",
           {
           bcOut <- calcBCmets(dfTest[[2]])
            compOut <- merge(testMets,bcOut,by=c('UID','PARAMETER'))
-           compOut <- dplyr::mutate(compOut,RESULT.x=as.numeric(RESULT.x))
+           compOut$RESULT.x <- with(compOut, as.numeric(RESULT.x))
            expect_true(nrow(bcOut)==20)
            expect_true(nrow(compOut)==nrow(bcOut))
            expect_equal(compOut$RESULT.x,compOut$RESULT.y,tolerance=0.001)
@@ -116,9 +116,12 @@ test_that("Mean Bray-Curtis metric values correct",
 test_that("VMMI metric calculations",
           {
            metOut <- calcVMMImets(testForCalc)
-           metOut.long <- reshape2::melt(metOut,id.vars='UID',variable.name='PARAMETER',value.name='RESULT')
+           varNames <- names(metOut)[names(metOut)!='UID']
+           metOut.long <- reshape(metOut, idvar = 'UID', direction = 'long',
+                                  times = varNames, varying = varNames,
+                                  timevar = 'PARAMETER', v.names = 'RESULT')
            compOut <- merge(testMets,metOut.long,by=c('UID','PARAMETER'))
-           compOut <- dplyr::mutate(compOut,RESULT.x=as.numeric(RESULT.x))
+           compOut$RESULT.x <- with(compOut, as.numeric(RESULT.x))
            expect_true(nrow(metOut.long)==40)
            expect_true(nrow(compOut)==nrow(metOut.long))
            expect_equal(compOut$RESULT.x,compOut$RESULT.y,tolerance=0.001)
@@ -128,9 +131,12 @@ test_that("All vascular plant metrics correct",
           {
             metOut <- calcVascPlantMets(testVasc,taxaIn=taxaNWCA,taxaNat=ccNatNWCA, 
                                         taxaCC=ccNatNWCA,taxaWIS=wisNWCA,sampID='UID', cValReg='STATE')
-            metOut.long <- reshape2::melt(metOut,id.vars='UID',variable.name='PARAMETER',value.name='RESULT')
+            varNames <- names(metOut)[names(metOut)!='UID']
+            metOut.long <- reshape(metOut, idvar = 'UID', direction = 'long',
+                                   times = varNames, varying = varNames,
+                                   timevar = 'PARAMETER', v.names = 'RESULT')
             compOut <- merge(testMets,metOut.long,by=c('UID','PARAMETER'))
-            compOut <- dplyr::mutate(compOut,RESULT.x=as.numeric(RESULT.x))
+            compOut$RESULT.x <- with(compOut, as.numeric(RESULT.x))
             expect_true(nrow(metOut.long)==3920)
             expect_true(nrow(metOut.long)==nrow(compOut))
             expect_equal(compOut$RESULT.x,compOut$RESULT.y,tolerance=0.001)
